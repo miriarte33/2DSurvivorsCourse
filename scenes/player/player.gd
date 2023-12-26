e67e39bcea91eebe6b632fb06c43ea9b0eb3c968
Manrique Iriarte <m_iriarte33@yahoo.com>
2023-12-26 14:58:10 -0500

    add main scene and refactor the player movement script

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -9,16 +9,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var movement_vector = get_movement_vector()
-	var direction = movement_vector.normalized()
-	
 	# velocity is a property of CharacterBody2D. F1 opens up the documentation in-engine
 	# this will send us at 200 pixels/second in the direction pressed
-	velocity = direction * MAX_SPEED
+	velocity = get_movement_velocity()
 	move_and_slide()
-	
 
-func get_movement_vector():
+
+# Gets the desired movement velocity from the direction the player is pressing
+func get_movement_velocity():
+	return get_direction() * MAX_SPEED
+
+# Returns a vector representing the direction the player wants to move
+func get_direction():
 	# get_action_stength always returns 0 or 1 with a keyboard input, it was either pressed or it wasn't
 	# if move_right is not pressed, and move_left is pressed, then x movement is -1
 	# if move_right is pressed, and move_left is not pressed, then x movement is 1
@@ -29,5 +31,5 @@ func get_movement_vector():
 	# down direction in y is positive, up is negative. So move_down should go first.
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	
-	# return the input state of x and y
-	return Vector2(x_movement, y_movement)
+	# return the normalized input state of x and y
+	return Vector2(x_movement, y_movement).normalized()
