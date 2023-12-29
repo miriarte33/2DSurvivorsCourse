79ea441f63291a48f47079e06019e69cc34859cb
Manrique Iriarte <m_iriarte33@yahoo.com>
2023-12-29 14:29:03 -0500

    spawn sword on the nearest enemy

--- scenes/abilities/sword_ability_controller/sword_ability_controller.gd
+++ scenes/abilities/sword_ability_controller/sword_ability_controller.gd
@@ -1,6 +1,9 @@
 extends Node
 
-# Export the sword ability scene to be able to spawn it at run time. 
+# The max range of the sword ability
+const MAX_RANGE = 150
+
+# Export the sword ability scene to be able to spawn it at run time.
 @export var sword_ability: PackedScene
 
 
@@ -10,14 +13,17 @@ func _ready():
 	# a sword when the timer runs out.
 	var timer = get_node("Timer") as Timer
 	timer.timeout.connect(on_timer_timeout)
-	
-	
+
+
 # Spawns a sword to the main scene whenever the timer times out
 func on_timer_timeout():
 	var player = Utils.get_player_node(self)
-	if (player == null):
+	if player == null:
 		return
-	
-	var sword_instance = sword_ability.instantiate() as Node2D	
+
+	var enemies = Utils.get_distance_sorted_enemies(self, MAX_RANGE)
+	var sword_instance = sword_ability.instantiate() as Node2D
+
+	# this line adds the sword instance to the main scene which is the parent of player
 	player.get_parent().add_child(sword_instance)
-	sword_instance.global_position = player.global_position
+	sword_instance.global_position = enemies[0].global_position
--- utils/utils.gd
+++ utils/utils.gd
@@ -1,13 +1,50 @@
 class_name Utils
 
-# Method that provides Frame independent lerp between 2 points 
-static func smoothe_lerp(a: Vector2, b: Vector2, time_between_frames: float, smoothing_value: int) -> Vector2:
+
+# Method that provides Frame independent lerp between 2 points
+static func smoothe_lerp(
+	a: Vector2,
+	b: Vector2,
+	time_between_frames: float,
+	smoothing_value: int,
+) -> Vector2:
 	# "1.0 - exp(-time_between_frames * smoothing_value)" is a commonly known formula that gives you a natural
 	# feeling percentage for camera smoothing to use when lerping between 2 points.
 	return a.lerp(b, 1.0 - exp(-time_between_frames * smoothing_value))
-	
-	
+
+
 # Gets the player node from the "player" group, which is the first node
 # in the group.
 static func get_player_node(current_node: Node) -> Node2D:
 	return current_node.get_tree().get_first_node_in_group("player")
+
+
+# Returns the distance between 2 vectors.
+static func distance_between(a: Vector2, b: Vector2) -> float:
+	# distance_squared_to is more efficient than distance_to
+	return a.distance_squared_to(b)
+
+
+# Returns enemies sorted in order of distance within the provided max range
+static func get_distance_sorted_enemies(node: Node, max_range: int) -> Array[Node]:
+	var player = get_player_node(node)
+	if player == null:
+		[]
+
+	var enemies = node.get_tree().get_nodes_in_group("enemies")
+	var within_max_range = func(enemy: Node2D): 
+		return Utils.distance_between(
+			enemy.global_position,
+			player.global_position) < pow(max_range, 2)
+			
+	var filteredEnemies = enemies.filter(within_max_range)
+	if filteredEnemies.size() == 0:
+		return []
+	
+	var sort_by_distance = func (a: Node2D, b: Node2D):
+		var a_distance = Utils.distance_between(a.global_position, player.global_position)
+		var b_distance = Utils.distance_between(b.global_position, player.global_position)
+		return a_distance < b_distance
+
+	filteredEnemies.sort_custom(sort_by_distance)
+	return filteredEnemies
