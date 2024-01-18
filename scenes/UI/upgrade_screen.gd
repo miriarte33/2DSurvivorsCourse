1ba034b8b7320fcd0536ab960d93d449b1f4fdbf
Manrique Iriarte <m_iriarte33@yahoo.com>
2024-01-17 22:00:16 -0500

    upgrade selection

--- scenes/UI/upgrade_screen.gd
+++ scenes/UI/upgrade_screen.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+signal upgrade_selected(upgrade: AbilityUpgrade)
+
 @export var ability_upgrade_card_scene: PackedScene
 
 @onready var card_container: HBoxContainer = $MarginContainer/CardContainer
@@ -9,8 +11,21 @@ func _ready():
 	get_tree().paused = true
 
 
-func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
+func set_upgrade_choices(upgrades: Array[AbilityUpgrade]):
 	for upgrade in upgrades:
 		var card_instance = ability_upgrade_card_scene.instantiate()
 		card_container.add_child(card_instance)
 		card_instance.set_ability_upgrade(upgrade)
+
+		# Bind lets you pass in a function with an arbitrary argument while still
+		# passing a reference to that function.
+		# This allows us to pass a function that takes in a function with an upgrade
+		# to connect to the selected signal even tho the selected signal does not take in
+		# any parameters.
+		card_instance.selected.connect(on_upgrade_selected.bind(upgrade))
+
+
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+	upgrade_selected.emit(upgrade)
+	get_tree().paused = false
+	queue_free()
--- scenes/managers/upgrade/upgrade_manager.gd
+++ scenes/managers/upgrade/upgrade_manager.gd
@@ -24,8 +24,12 @@ func on_level_up(_current_level: int):
 
 	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
 	add_child(upgrade_screen_instance)
-	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
-	#apply_upgrade(chosen_upgrade)
+	upgrade_screen_instance.set_upgrade_choices([chosen_upgrade] as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
+
+
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+	apply_upgrade(upgrade)
 
 
 func apply_upgrade(upgrade: AbilityUpgrade):
@@ -35,3 +39,5 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 		current_upgrades[upgrade.id] = {"resource": upgrade, "quantity": 1}
 	else:
 		current_upgrades[upgrade.id]["quantity"] += 1
+
+	print(current_upgrades)
